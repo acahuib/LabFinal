d9e4aa9bbb1cb3009c1d0fe28b5cac46de57eda3
Anthony <acahuib@unsa.edu.pe>
2024-07-06 14:31:56 -0500

    Se configura el api para ser mostrado correctamente al ejecutar el server

--- rental_app/rentals/models.py
+++ rental_app/rentals/models.py
@@ -1,4 +1,3 @@
-# rentals/models.py
 from django.db import models
 
 class Costume(models.Model):
@@ -12,9 +11,14 @@ class Costume(models.Model):
 class Rental(models.Model):
     costume = models.ForeignKey(Costume, on_delete=models.CASCADE)
     customer_name = models.CharField(max_length=100)
-    rental_date = models.DateTimeField(auto_now_add=True)
+    rental_date = models.DateTimeField()
     return_date = models.DateTimeField(null=True, blank=True)
     returned = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if not self.id:  # Nuevo objeto
+            self.rental_date = timezone.now()  # Establecer la fecha de alquiler al crear
+        super(Rental, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.customer_name} - {self.costume.name}"
--- rental_app/rentals/views.py
+++ rental_app/rentals/views.py
@@ -1,59 +1,51 @@
-from django.shortcuts import render, get_object_or_404
+# rentals/views.py
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Costume, Rental
+from .forms import CostumeForm, RentalForm
 
 class CostumeListView(ListView):
     model = Costume
-    template_name = 'rentals/costume_list.html'  # Crear esta plantilla luego
-    context_object_name = 'costumes'
 
 class CostumeDetailView(DetailView):
     model = Costume
-    template_name = 'rentals/costume_detail.html'  # Crear esta plantilla luego
-    context_object_name = 'costume'
 
 class CostumeCreateView(CreateView):
     model = Costume
-    template_name = 'rentals/costume_form.html'  # Crear esta plantilla luego
-    fields = ['name', 'description', 'available']
+    template_name = 'rentals/costume_form.html'
+    form_class = CostumeForm
     success_url = reverse_lazy('costume-list')
 
 class CostumeUpdateView(UpdateView):
     model = Costume
-    template_name = 'rentals/costume_form.html'  # Crear esta plantilla luego
-    fields = ['name', 'description', 'available']
+    template_name = 'rentals/costume_form.html'
+    form_class = CostumeForm
     success_url = reverse_lazy('costume-list')
 
 class CostumeDeleteView(DeleteView):
     model = Costume
-    template_name = 'rentals/costume_confirm_delete.html'  # Crear esta plantilla luego
     success_url = reverse_lazy('costume-list')
 
 class RentalListView(ListView):
     model = Rental
-    template_name = 'rentals/rental_list.html'  # Crear esta plantilla luego
-    context_object_name = 'rentals'
 
 class RentalDetailView(DetailView):
     model = Rental
-    template_name = 'rentals/rental_detail.html'  # Crear esta plantilla luego
-    context_object_name = 'rental'
 
 class RentalCreateView(CreateView):
     model = Rental
-    template_name = 'rentals/rental_form.html'  # Crear esta plantilla luego
-    fields = ['costume', 'customer_name', 'rental_date', 'return_date', 'returned']
+    template_name = 'rentals/rental_form.html'
+    form_class = RentalForm
     success_url = reverse_lazy('rental-list')
 
 class RentalUpdateView(UpdateView):
     model = Rental
-    template_name = 'rentals/rental_form.html'  # Crear esta plantilla luego
-    fields = ['costume', 'customer_name', 'rental_date', 'return_date', 'returned']
+    template_name = 'rentals/rental_form.html'
+    form_class = RentalForm
     success_url = reverse_lazy('rental-list')
 
 class RentalDeleteView(DeleteView):
     model = Rental
-    template_name = 'rentals/rental_confirm_delete.html'  # Crear esta plantilla luego
     success_url = reverse_lazy('rental-list')
 
