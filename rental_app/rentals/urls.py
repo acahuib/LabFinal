a1cf3174c15d8bbd609a61abb55657fbe68a0fd8
Anthony <acahuib@unsa.edu.pe>
2024-07-06 14:31:13 -0500

    Por implementar el api se agregara nuevas urls para el api

--- rental_app/rentals/urls.py
+++ rental_app/rentals/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .api_views import CostumeViewSet, RentalViewSet
 from . import views
 
+router = DefaultRouter()
+router.register(r'costumes', CostumeViewSet)
+router.register(r'rentals', RentalViewSet)
+
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('costumes/', views.CostumeListView.as_view(), name='costume-list'),
     path('costumes/<int:pk>/', views.CostumeDetailView.as_view(), name='costume-detail'),
     path('costumes/new/', views.CostumeCreateView.as_view(), name='costume-create'),
