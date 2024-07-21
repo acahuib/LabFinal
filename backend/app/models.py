0cc9d3b54ab57441cb599350e92adf31d78aab59
Anthony <acahuib@unsa.edu.pe>
2024-07-21 18:07:17 -0500

    Se agrego una funcion mas de crud para poder modificar los generos de la pelicula en el django

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Movie(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     image = models.URLField()
     rating = models.IntegerField(default=0)
-    genres = models.JSONField()
+    genres = models.ManyToManyField(Genre, related_name='movies')  # Relación de muchos a muchos con Genre
     in_theaters = models.BooleanField(default=False)
 
     def __str__(self):
--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MovieViewSet
+from .views import MovieViewSet, GenreViewSet
 
 router = DefaultRouter()
 router.register(r'movies', MovieViewSet)
+router.register(r'genres', GenreViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
